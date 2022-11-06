0a163b547e351db482b7d5799f758fa09bef5ff7
gwg1999 <58027253+gwg1999@users.noreply.github.com>
2022-11-06 20:38:15 +0800

    update:更新页面局部代码（未完善）

---
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="menus"></div>
+</template>
+
+<script>
+export default {
+  name: "SidebarItem",
+  props: {
+    menus: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-scrollbar>
+      <el-menu
+        :collapse="false"
+        :unique-opened="false"
+        :default-active="activeMenu"
+      >
+        <sidebar-item v-for="route in menus" :key="route.path" :item="route"></sidebar-item>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import SidebarItem from "./SidebarItem";
+export default {
+  name: "Sidebar",
+  components: {SidebarItem},
+  props: {
+    menus: {
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  },
+  computed: {
+    activeMenu(){
+      const route = this.$route
+      const {meta, path} = route
+      if(meta.activeMenu) {
+        return meta.activeMenu
+      }
+      return path
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
